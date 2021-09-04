9aef9e3fc54e1363c0cc1bd478ee54ef2bfde36c
HashLips <88177839+HashLips@users.noreply.github.com>
2021-09-04 12:30:22 +0200

    Added files

--- contracts/SmartContract.sol
+++ contracts/SmartContract.sol
@@ -1,28 +1,37 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+
+/**
+ 
+ *Luna Landers by LunaLand (LLN)
+ *
+ * 10,000 Lunalanders flying through the Metaverse.
+*/
 
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmartContract is ERC721Enumerable, Ownable {
+contract LunaLanders is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string public baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0.02 ether;
+  uint256 public cost = 0.05 ether;
   uint256 public maxSupply = 10000;
   uint256 public maxMintAmount = 20;
   bool public paused = false;
+  mapping(address => bool) public whitelisted;
 
   constructor(
     string memory _name,
     string memory _symbol,
     string memory _initBaseURI
   ) ERC721(_name, _symbol) {
+     
     setBaseURI(_initBaseURI);
-
-    mint(msg.sender, 20);
+   
+    mint(msg.sender, 3);
   }
 
   // internal
@@ -39,7 +48,9 @@ contract SmartContract is ERC721Enumerable, Ownable {
     require(supply + _mintAmount <= maxSupply);
 
     if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount);
+        if(whitelisted[msg.sender] != true) {
+          require(msg.value >= cost * _mintAmount);
+        }
     }
 
     for (uint256 i = 1; i <= _mintAmount; i++) {
@@ -73,11 +84,8 @@ contract SmartContract is ERC721Enumerable, Ownable {
     );
 
     string memory currentBaseURI = _baseURI();
-    return
-      bytes(currentBaseURI).length > 0
-        ? string(
-          abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)
-        )
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
         : "";
   }
 
@@ -100,6 +108,14 @@ contract SmartContract is ERC721Enumerable, Ownable {
 
   function pause(bool _state) public onlyOwner {
     paused = _state;
+  }
+ 
+ function whitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = true;
+  }
+ 
+  function removeWhitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = false;
   }
 
   function withdraw() public payable onlyOwner {
--- migrations/2_smart_contract_migration.js
+++ migrations/2_smart_contract_migration.js
@@ -1,4 +1,4 @@
-const SmartContract = artifacts.require("SmartContract");
+const SmartContract = artifacts.require("LunaLanders");
 
 module.exports = function (deployer) {
   deployer.deploy(SmartContract, "Name", "Symbol", "https://");
