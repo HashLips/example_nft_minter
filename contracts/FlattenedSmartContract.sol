bc2efc25895e7937946a0e6b8ac5410537a5028d
HashLips <88177839+HashLips@users.noreply.github.com>
2021-10-20 09:11:30 +0200

    Updated

--- contracts/FlattenedSmartContract.sol
+++ contracts/FlattenedSmartContract.sol
@@ -1304,11 +1304,11 @@ contract NerdyCoderClones is ERC721Enumerable, Ownable {
   }
 
   //only owner
-  function setCost(uint256 _newCost) public onlyOwner() {
+  function setCost(uint256 _newCost) public onlyOwner {
     cost = _newCost;
   }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
     maxMintAmount = _newmaxMintAmount;
   }
 
